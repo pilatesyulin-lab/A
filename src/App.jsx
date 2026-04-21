fd1dbf8dda739399c376c77ee1e6941593d5a9cf
DESKTOP-UR65G9F\EZEN <dbtmzpsla11@naver.com>
2026-04-21 17:40:17 +0900

    .main으로  전체 다시 감쌋다. 라우터 경로 수정 / 추후에 리덕스 프로바이더 메인에 추가할 예정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // Routes, Route 추가
+import { Routes, Route } from "react-router-dom"; // Routes, Route 추가
 import GymListPage from "./pages/GymListPage";
 import GymDetailPage from "./pages/GymDetailPage"; // 상세 페이지 컴포넌트 임포트
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 메인 목록 페이지 경로 */}
-        <Route path="/" element={<GymListPage />} />
+    <Routes>
+      {/* 메인 목록 페이지 경로 */}
+      <Route path="/" element={<GymListPage />} />
 
-        {/* 상세 페이지 경로 (:id는 헬스장 번호에 따라 변하는 동적 값입니다) */}
-        <Route path="/gym/:id" element={<GymDetailPage />} />
-      </Routes>
-    </BrowserRouter>
+      {/* 상세 페이지 경로 (:id는 헬스장 번호에 따라 변하는 동적 값입니다) */}
+      <Route path="/gym/:id" element={<GymDetailPage />} />
+    </Routes>
   );
 }
 
